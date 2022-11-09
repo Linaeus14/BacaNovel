09e5eb3db1d88b6315b85f52c1a80a14b319b23f
Firdaus1223 <muhammadfirdaus41208@gmail.com>
2022-11-09 22:45:43 +0800

    Tampilan About Us

--- nav/me.php
+++ nav/me.php
@@ -13,15 +13,46 @@
     <a href="../php/logout.php" id="Logout"> Log Out </a>
 </nav>
 <main>
-    <div class="me">
-        <h1> Tito Darmawan </h1>
-        <p><strong> NIM : 2109106042 </strong></p>
-        <hr><p>
-        <h1> M. Firdaus </h1>
-        <p><strong> NIM : 2109106052 </strong></p>
-        <hr><p>
-        <h1> Ayu Lestari Gunawan </h1>
-        <p><strong> NIM : 2109106054 </strong></p>
+<h2 style="text-align:center">About Us</h2>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img class="zoom" src="../AboutUs/tito.jpeg" alt="Tito" style="width:100%">
+      <div  class="container">
+        <h2>Tito Darmawan</h2>
+        <p class="title">Ketua</p>
+        <p>2109106042</p>
+        <p>TitoDarmawan@gmail.com</p>
+      </div>
     </div>
+  </div>
+
+  <div class="column">
+    <div class="card">
+      <img class="zoom" src="../AboutUs/ayu.jpeg" alt="Ayu" style="width:100%">
+      <div class="container">
+        <h2>Ayu Lestari Gunawan</h2>
+        <p class="title">Anggota</p>
+        <p>2109106054</p>
+        <p>AyuLestariGunawan@gmail.com</p>
+      </div>
+    </div>
+  </div>
+  
+  <div class="column">
+    <div class="card">
+      <img class="zoom" src="../AboutUs/daus.jpeg" alt="Firdaus" style="width:100%">
+      <div class="container">
+        <h2>Muhammad Firdaus</h2>
+        <p class="title">Anggota</p>
+        <p>2109106052</p>
+        <p>MuhammadFirdaus@gmail.com</p>
+      </div>
+    </div>
+  </div>
+</div>
+
 </main>
 <?php require "../php/footer.php"?>  
